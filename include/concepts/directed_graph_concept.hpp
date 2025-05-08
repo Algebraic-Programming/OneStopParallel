d7415ac38ce74dcfaaf82eced0ee7ca7cbade974
tonibohnlein <toni.boehnlein18@gmail.com>
2025-05-08 20:25:57 +0200

    removed copy_assignale and move_assignale from directed_graph_concept

--- include/concepts/directed_graph_concept.hpp
+++ include/concepts/directed_graph_concept.hpp
@@ -46,9 +46,10 @@ struct is_directed_graph<
           std::is_unsigned<decltype(std::declval<T>().out_degree(std::declval<vertex_idx_t<T>>()))>,
           std::is_default_constructible<T>,
           std::is_move_constructible<T>,
-          std::is_copy_constructible<T>,
-          std::is_copy_assignable<T>,
-          std::is_move_assignable<T>> {};
+          std::is_copy_constructible<T>
+          //std::is_copy_assignable<T>
+          //std::is_move_assignable<T>
+          > {};
 
 template<typename T>
 inline constexpr bool is_directed_graph_v = is_directed_graph<T>::value;
