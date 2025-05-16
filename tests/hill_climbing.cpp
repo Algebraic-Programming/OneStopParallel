663a7b819eff2bab2cacac720d0c886b36be7569
ppapp <pal.andras.papp@huawei.com>
2025-05-16 14:46:12 +0200

    hill climbing for comm schedule adapted

--- tests/hill_climbing.cpp
+++ tests/hill_climbing.cpp
@@ -16,11 +16,12 @@ limitations under the License.
 @author Toni Boehnlein, Benjamin Lozes, Pal Andras Papp, Raphael S. Steiner
 */
 
-#define BOOST_TEST_MODULE kl
+#define BOOST_TEST_MODULE HILL_CLIMBING
 #include <boost/test/unit_test.hpp>
 
 #include "bsp/scheduler/GreedySchedulers/GreedyBspScheduler.hpp"
 #include "bsp/scheduler/LocalSearch/HillClimbing/hill_climbing.hpp"
+#include "bsp/scheduler/LocalSearch/HillClimbing/hill_climbing_for_comm_schedule.hpp"
 #include "io/hdag_graph_file_reader.hpp"
 #include <filesystem>
 
@@ -62,21 +63,70 @@ BOOST_AUTO_TEST_CASE(hill_climbing) {
 
     HillClimbingScheduler<graph> scheduler;
     BspSchedule<graph> schedule1 = bsp_initial;
-    scheduler.improveSchedule(bsp_initial);
+    scheduler.improveSchedule(schedule1);
     BOOST_CHECK_EQUAL(schedule1.satisfiesPrecedenceConstraints(), true);
 
     scheduler.setSteepestAscend(true);
     BspSchedule<graph> schedule2 = bsp_initial;
-    scheduler.improveSchedule(bsp_initial);
+    scheduler.improveSchedule(schedule2);
     BOOST_CHECK_EQUAL(schedule2.satisfiesPrecedenceConstraints(), true);
 
     BspSchedule<graph> schedule3 = bsp_initial;
     scheduler.setTimeLimitSeconds(1U);
-    scheduler.improveScheduleWithTimeLimit(bsp_initial);
+    scheduler.improveScheduleWithTimeLimit(schedule3);
     BOOST_CHECK_EQUAL(schedule3.satisfiesPrecedenceConstraints(), true);
 
     BspSchedule<graph> schedule4 = bsp_initial;
-    scheduler.improveScheduleWithStepLimit(bsp_initial, 5);
+    scheduler.improveScheduleWithStepLimit(schedule4, 5);
     BOOST_CHECK_EQUAL(schedule4.satisfiesPrecedenceConstraints(), true);
+
+};
+
+BOOST_AUTO_TEST_CASE(hill_climbing_for_comm_schedule) {
+
+    using graph = computational_dag_vector_impl_def_t;
+
+    BspInstance<graph> instance;
+    instance.setNumberOfProcessors(2);
+    instance.setCommunicationCosts(3);
+    instance.setSynchronisationCosts(5);
+
+    // Getting root git directory
+    std::filesystem::path cwd = std::filesystem::current_path();
+    std::cout << cwd << std::endl;
+    while ((!cwd.empty()) && (cwd.filename() != "OneStopParallel")) {
+        cwd = cwd.parent_path();
+        std::cout << cwd << std::endl;
+    }
+
+    bool status = file_reader::readComputationalDagHyperdagFormat(
+        (cwd / "data/spaa/tiny/instance_CG_N4_K1_nzP0d35.hdag").string(), instance.getComputationalDag());
+
+    BOOST_CHECK(status);
+
+    GreedyBspScheduler<graph> greedy;
+    BspSchedule<graph> initial(instance);
+    BOOST_CHECK_EQUAL(SUCCESS, greedy.computeSchedule(initial));
+    BOOST_CHECK_EQUAL(initial.satisfiesPrecedenceConstraints(), true);
+
+    HillClimbingScheduler<graph> hc;
+    hc.improveSchedule(initial);
+    BOOST_CHECK_EQUAL(initial.satisfiesPrecedenceConstraints(), true);
+
+    BspSchedule<graph> schedule = initial;
+    BspScheduleCS<graph> initial_cs(std::move(initial));
+    //initial_cs.setAutoCommunicationSchedule();
+    initial_cs.setEagerCommunicationSchedule();
+    BOOST_CHECK_EQUAL(initial_cs.hasValidCommSchedule(), true);
+
+    HillClimbingForCommSteps<graph> hc_cs;
+    BspScheduleCS<graph> schedule1 = initial_cs;
+    hc_cs.improveSchedule(schedule1);
+    BOOST_CHECK_EQUAL(schedule1.hasValidCommSchedule(), true);
+
+    BspScheduleCS<graph> schedule2 = initial_cs;
+    hc_cs.setSteepestAscend(true);
+    hc_cs.improveSchedule(schedule2);
+    BOOST_CHECK_EQUAL(schedule2.hasValidCommSchedule(), true);
 
 };
