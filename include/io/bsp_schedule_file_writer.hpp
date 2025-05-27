0ba0ca23f2b4a2b0a96027eba01e40eb60a3f439
tonibohnlein <toni.boehnlein18@gmail.com>
2025-05-27 13:56:24 +0200

    added sankey sched writer

--- include/io/bsp_schedule_file_writer.hpp
+++ include/io/bsp_schedule_file_writer.hpp
@@ -83,4 +83,59 @@ void write_txt(const std::string &filename, const BspScheduleCS<Graph_t> &schedu
     write_txt(os, schedule);
 }
 
+template<typename Graph_t>
+void write_sankey(std::ostream &os, const BspScheduleCS<Graph_t> &schedule) {
+    // Computing workloads
+    std::vector<std::vector<v_workw_t<Graph_t>>> proc_workloads(
+        schedule.numberOfSupersteps(), std::vector<v_workw_t<Graph_t>>(schedule.getInstance().numberOfProcessors(), 0));
+
+    for (size_t node = 0; node < schedule.getInstance().numberOfVertices(); node++) {
+        proc_workloads[schedule.assignedSuperstep(node)][schedule.assignedProcessor(node)] +=
+            schedule.getInstance().getComputationalDag().vertex_work_weight(node);
+    }
+
+    // Computing communicationloads
+    std::vector<std::vector<std::vector<v_commw_t<Graph_t>>>> commloads(
+                      schedule.numberOfSupersteps() - 1,
+                      std::vector<std::vector<v_commw_t<Graph_t>>>(
+                          schedule.getInstance().numberOfProcessors(),
+                          std::vector<v_commw_t<Graph_t>>(schedule.getInstance().numberOfProcessors(), 0)));
+
+    for (const auto &[comm_triple, sstep] : schedule.getCommunicationSchedule()) {
+        commloads[sstep][std::get<1>(comm_triple)][std::get<2>(comm_triple)] +=
+            schedule.getInstance().getComputationalDag().vertex_comm_weight(std::get<0>(comm_triple));
+    }
+
+    os << "BspSchedule: Number of Processors, Number of Supersteps" << std::endl;
+    os << schedule.getInstance().numberOfProcessors() << "," << schedule.numberOfSupersteps() << std::endl;
+
+    os << "Processor workloads in Superstep" << std::endl;
+    for (const auto &sstep : proc_workloads) {
+        for (size_t proc_ind = 0; proc_ind < sstep.size(); proc_ind++) {
+            if (proc_ind != 0) {
+                os << ",";
+            }
+            os << sstep[proc_ind];
+        }
+        os << std::endl;
+    }
+
+    os << "Communication between Processors in Supersteps" << std::endl;
+    for (size_t sstep = 0; sstep < commloads.size(); sstep++) {
+        for (size_t send_proc = 0; send_proc < schedule.getInstance().numberOfProcessors(); send_proc++) {
+            for (size_t receive_proc = 0; receive_proc < schedule.getInstance().numberOfProcessors(); receive_proc++) {
+                // if (commloads[ sstep ][ send_proc ][ receive_proc ] == 0) continue;
+                os << sstep + 1 << "," << send_proc + 1 << "," << receive_proc + 1 << ","
+                   << commloads[sstep][send_proc][receive_proc] << std::endl;
+            }
+        }
+    }
+}
+
+template<typename Graph_t>
+void write_sankey(const std::string &filename, const BspScheduleCS<Graph_t> &schedule) {
+    std::ofstream os(filename);
+    write_sankey(os, schedule);
+}
+
 }} // namespace osp::file_writer
