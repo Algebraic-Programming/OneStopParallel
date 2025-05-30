d57ceb289b30236c08609c8394bb23a5e9efca81
tonibohnlein <toni.boehnlein18@gmail.com>
2025-05-30 11:37:51 +0200

    added long_edges_in_triangles_parallel + test

---
+++ include/graph_algorithms/directed_graph_edge_desc_util_parallel.hpp
@@ -0,0 +1,77 @@
+/*
+Copyright 2024 Huawei Technologies Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author Toni Boehnlein, Benjamin Lozes, Pal Andras Papp, Raphael S. Steiner
+*/
+
+#pragma once
+
+#include <omp.h>
+#include <queue>
+#include <unordered_set>
+#include <vector>
+
+#include "concepts/directed_graph_concept.hpp"
+#include "directed_graph_edge_desc_util.hpp"
+
+namespace osp {
+
+template<typename Graph_t>
+std::unordered_set<edge_desc_t<Graph_t>> long_edges_in_triangles_parallel(const Graph_t &graph) {
+
+    static_assert(is_directed_graph_edge_desc_v<Graph_t>, "Graph_t must satisfy the directed_graph edge desc concept");
+    static_assert(has_hashable_edge_desc_v<Graph_t>, "Graph_t must satisfy the has_hashable_edge_desc concept");
+
+    if (graph.num_edges() < 1000) {
+        return long_edges_in_triangles(graph);
+    }
+
+    std::unordered_set<edge_desc_t<Graph_t>> long_edges;
+    std::vector<std::vector<edge_desc_t<Graph_t>>> deleted_edges_thread(static_cast<size_t>(omp_get_max_threads()));
+
+#pragma omp parallel for schedule(dynamic, 4)
+    for (const auto &vertex : graph.vertices()) {
+
+        const unsigned int proc = static_cast<unsigned>(omp_get_thread_num());
+
+        std::unordered_set<vertex_idx_t<Graph_t>> children_set;
+        for (const auto &v : graph.children(vertex)) {
+            children_set.emplace(v);
+        }
+
+        for (const auto &edge : graph.out_edges(vertex)) {
+
+            const auto &child = target(edge, graph);
+
+            for (const auto &parent : graph.parents(child)) {
+
+                if (children_set.find(parent) != children_set.cend()) {
+                    deleted_edges_thread[proc].emplace_back(edge);
+                    break;
+                }
+            }
+        }
+    }
+
+    for (const auto &edges_thread : deleted_edges_thread) {
+        for (const auto &edge : edges_thread) {
+            long_edges.emplace(edge);
+        }
+    }
+
+    return long_edges;
+}
+
+} // namespace osp
