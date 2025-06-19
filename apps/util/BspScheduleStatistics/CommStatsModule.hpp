359c90efdd062ec40f16e980f7fe69af34f0c1ea
tonibohnlein <toni.boehnlein18@gmail.com>
2025-06-19 13:50:28 +0200

    test-update

---
+++ apps/util/BspScheduleStatistics/CommStatsModule.hpp
@@ -0,0 +1,54 @@
+/*
+Copyright 2024 Huawei Technologies Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author Toni Boehnlein, Benjamin Lozes, Pal Andras Papp, Raphael S. Steiner
+*/
+
+#pragma once
+
+#include "IStatisticModule.hpp"
+#include "bsp/model/BspSchedule.hpp"
+#include <string>
+#include <vector>
+#include <map>
+
+namespace osp {
+
+template<typename Graph_t_>
+class CommStatsModule : public IStatisticModule<Graph_t_> { 
+public:
+    using Graph_t = Graph_t_;
+
+private:
+    const std::vector<std::string> metric_headers = {
+        "TotalCommCost", "BufferedSendingCosts"
+    };
+
+public:
+    std::vector<std::string> get_metric_headers() const override {
+        return metric_headers;
+    }
+
+    std::map<std::string, std::string> record_statistics(
+                            const osp::BspSchedule<Graph_t>& schedule,
+                            std::ofstream& /*log_stream*/) const override {
+        std::map<std::string, std::string> stats;
+        stats["TotalCommCost"] = std::to_string(schedule.computeCostsTotalCommunication());
+        stats["BufferedSendingCosts"] = std::to_string(schedule.computeCostsBufferedSending());
+        return stats;
+    }
+};
+
+} // namespace osp
