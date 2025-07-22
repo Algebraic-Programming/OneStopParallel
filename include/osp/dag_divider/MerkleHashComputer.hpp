b044d2feda4a0b27a71d6036383392d7389d2797
tonibohnlein <toni.boehnlein18@gmail.com>
2025-07-22 12:31:29 +0200

    added MerkleHashComputer.hpp

---
+++ include/osp/dag_divider/MerkleHashComputer.hpp
@@ -0,0 +1,103 @@
+/*
+Copyright 2024 Huawei Technologies Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author Toni Boehnlein, Benjamin Lozes, Pal Andras Papp, Raphael S. Steiner
+*/
+
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <stdexcept> 
+#include "osp/concepts/computational_dag_concept.hpp"
+#include "osp/graph_algorithms/directed_graph_top_sort.hpp"
+#include "osp/auxiliary/misc.hpp"
+
+namespace osp {
+
+template<typename VertexType>
+struct default_node_hash_func {
+
+    std::size_t operator()(const VertexType& ) {
+        return 11;
+    }
+
+};
+
+template<typename Graph_t, typename node_hash_func_t = default_node_hash_func<vertex_idx_t<Graph_t>>>
+class MerkleHashComputer {
+
+    using VertexType = vertex_idx_t<Graph_t>;
+
+    std::vector<std::size_t> vertex_hashes;
+    std::unordered_map<std::size_t, std::vector<VertexType>> orbits;
+
+    node_hash_func_t node_hash_func;
+
+    void compute_hashes(const Graph_t & graph) {
+
+        vertex_hashes.resize(graph.num_vertices());
+        
+        for (const VertexType &v : top_sort_view(graph)) {
+
+            std::vector<std::size_t> parent_hashes;
+            for (const VertexType& parent : graph.parents(v)) {
+                parent_hashes.push_back(vertex_hashes[parent]);
+            }
+
+            std::sort(parent_hashes.begin(),parent_hashes.end());
+
+            std::size_t hash = node_hash_func(v);
+            for (const VertexType& parent_hash : parent_hashes) {
+                hash_combine(hash, parent_hash); 
+            }
+   
+            vertex_hashes[v] = hash;
+
+            if (orbits.find(hash) == orbits.end()) {
+                orbits[hash] = {v};
+            } else {
+                orbits[hash].push_back(v);
+            }
+        }
+    }
+
+  public:
+
+    MerkleHashComputer(const Graph_t & g) {
+        compute_hashes(g);
+    }
+    virtual ~MerkleHashComputer() = default;
+
+    std::size_t get_vertex_hash(const VertexType &v) { return vertex_hashes[v]; }
+    const std::vector<std::size_t> &get_vertex_hashes() { return vertex_hashes; }
+    std::size_t num_orbits() { return orbits.size(); }
+    
+    const std::vector<VertexType> &get_orbit(const VertexType &v) { return get_orbit_from_hash(get_vertex_hash(v)); }
+    const std::unordered_map<std::size_t, std::vector<VertexType>> &get_orbits() { return orbits; }
+
+    const std::vector<VertexType>& get_orbit_from_hash(const std::size_t& hash) const {
+    try {
+        return orbits.at(hash);
+    } catch (const std::out_of_range& oor) {
+        throw std::out_of_range("No orbit found for the given hash.");
+    }
+}
+
+};
+
+
+} // namespace osp
---
+++ tests/merkle_hash_computer.cpp
@@ -0,0 +1,77 @@
+/*
+Copyright 2024 Huawei Technologies Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author Toni Boehnlein, Benjamin Lozes, Pal Andras Papp, Raphael S. Steiner
+*/
+
+#define BOOST_TEST_MODULE BSP_SCHEDULE_RECOMP
+#include <boost/test/unit_test.hpp>
+
+#include "osp/dag_divider/MerkleHashComputer.hpp"
+#include "osp/graph_algorithms/directed_graph_util.hpp"
+#include "osp/graph_implementations/adj_list_impl/computational_dag_vector_impl.hpp"
+#include "osp/auxiliary/io/hdag_graph_file_reader.hpp"
+#include <filesystem>
+#include <iostream>
+
+using namespace osp;
+
+BOOST_AUTO_TEST_CASE(BspScheduleRecomp_test)
+{
+
+    using graph_t = computational_dag_vector_impl_def_t;
+
+    graph_t graph;
+
+    // Getting root git directory
+    std::filesystem::path cwd = std::filesystem::current_path();
+    std::cout << cwd << std::endl;
+    while ((!cwd.empty()) && (cwd.filename() != "OneStopParallel")) {
+        cwd = cwd.parent_path();
+        std::cout << cwd << std::endl;
+    }
+
+    file_reader::readComputationalDagHyperdagFormat((cwd / "data/spaa/tiny/instance_bicgstab.hdag").string(), graph);
+
+    MerkleHashComputer m_hash(graph);
+
+    BOOST_CHECK_EQUAL(m_hash.get_vertex_hashes().size(), graph.num_vertices());
+    
+    for (const auto& v : source_vertices_view(graph)) {
+        BOOST_CHECK_EQUAL(m_hash.get_vertex_hash(v), 11);
+    }
+
+    size_t num = 0;
+    for (const auto& pair : m_hash.get_orbits()) {
+
+        num += pair.second.size();
+        std::cout << "orbit " << pair.first << ": ";
+        for (const auto& v : pair.second) {
+            std::cout << v << ", ";
+        } 
+        std::cout << std::endl;
+    }
+
+    BOOST_CHECK_EQUAL(num, graph.num_vertices());
+
+    BOOST_CHECK_EQUAL(m_hash.get_vertex_hash(41), m_hash.get_vertex_hash(47));
+    BOOST_CHECK_EQUAL(m_hash.get_vertex_hash(28), m_hash.get_vertex_hash(18));
+    BOOST_CHECK_EQUAL(m_hash.get_vertex_hash(43), m_hash.get_vertex_hash(48));
+    BOOST_CHECK_EQUAL(m_hash.get_vertex_hash(29), m_hash.get_vertex_hash(22));
+    BOOST_CHECK(m_hash.get_vertex_hash(3) != m_hash.get_vertex_hash(12));
+    BOOST_CHECK(m_hash.get_vertex_hash(53) != m_hash.get_vertex_hash(29));
+
+
+};
