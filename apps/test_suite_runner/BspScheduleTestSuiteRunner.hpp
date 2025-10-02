75de7cfe2932f1a2139e7efdde664f7b7784ef9a
Christos Konstantinos Matzoros <christos.konstantinos.matzoros@h-partners.com>
2025-10-02 14:40:45 +0200

    Minor bug fix (adding ifdef eigen for a part of a runner)

--- apps/test_suite_runner/BspScheduleTestSuiteRunner.hpp
+++ apps/test_suite_runner/BspScheduleTestSuiteRunner.hpp
@@ -55,12 +55,14 @@ class BspScheduleTestSuiteRunner : public AbstractTestSuiteRunner<BspSchedule<co
             this->active_stats_modules.push_back(std::make_unique<BasicBspStatsModule<BspSchedule<concrete_graph_t>>>());
         } else if (module_name == "BspCommStats") {
             this->active_stats_modules.push_back(std::make_unique<BspCommStatsModule<concrete_graph_t>>());
+#ifdef EIGEN_FOUND
         } else if (module_name == "BspSptrsvStats") {
             this->active_stats_modules.push_back(std::make_unique<BspSptrsvStatsModule<BspSchedule<concrete_graph_t>>>(NO_PERMUTE));
         } else if (module_name == "BspSptrsvPermLoopProcessorsStats") {
             this->active_stats_modules.push_back(std::make_unique<BspSptrsvStatsModule<BspSchedule<concrete_graph_t>>>(LOOP_PROCESSORS));
         } else if (module_name == "BspSptrsvPermSnakeProcessorsStats") {
             this->active_stats_modules.push_back(std::make_unique<BspSptrsvStatsModule<BspSchedule<concrete_graph_t>>>(SNAKE_PROCESSORS));
+#endif
         } else if (module_name == "GraphStats") {
             this->active_stats_modules.push_back(
                 std::make_unique<GraphStatsModule<BspSchedule<concrete_graph_t>>>());
