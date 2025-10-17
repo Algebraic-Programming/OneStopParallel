4f7072a14d5a5dd3d1cd1c8b82aec756369b9466
Christos Konstantinos Matzoros <christos.konstantinos.matzoros@h-partners.com>
2025-10-17 09:56:08 +0200

    Add spack module recipe for OneStopParallel

---
+++ spack/package.py
@@ -0,0 +1,38 @@
+from spack.package import *
+
+
+class Onestopparallel(CMakePackage):
+    """OneStopParallel (OSP): This project aims to develop scheduling algorithms 
+    for parallel computing systems based on the Bulk Synchronous Parallel (BSP) model. 
+    The algorithms optimize the allocation of tasks to processors, taking into 
+    account factors such as load balancing, memory constraints and communication overhead."""
+
+    homepage = "https://github.com/Algebraic-Programming/OneStopParallel"
+    git      = "https://github.com/Algebraic-Programming/OneStopParallel.git"
+
+    maintainers = ['cmatzoros']
+
+    version('master', branch='master')
+
+    # Dependencies
+    depends_on('cmake@3.12:', type='build')
+    depends_on('boost@1.71.0:+graph+test', type=('build', 'link'))
+    depends_on('eigen@3.4.0:', type=('build', 'link'))
+
+
+    variant('openmp', default=True, description='Enable OpenMP support')
+
+    def cmake_args(self):
+        args = []
+
+        # build the library version only
+        args.append('-DCMAKE_BUILD_TYPE=Library')
+        args.append('-DBUILD_TESTS=OFF')
+
+        if '+openmp' in self.spec:
+            args.append('-DOSP_DEPENDS_ON_OPENMP=ON')
+        else:
+            args.append('-DOSP_DEPENDS_ON_OPENMP=OFF')
+
+        args.append('-DCMAKE_INSTALL_PREFIX={0}'.format(self.prefix))
+        return args
