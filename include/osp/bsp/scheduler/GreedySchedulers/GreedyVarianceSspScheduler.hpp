c998da845dcc8bba0728ed677216d8070ca31777
Christos Konstantinos Matzoros <christos.konstantinos.matzoros@h-partners.com>
2025-11-17 15:29:36 +0100

    Fix cast warning

--- include/osp/bsp/scheduler/GreedySchedulers/GreedyVarianceSspScheduler.hpp
+++ include/osp/bsp/scheduler/GreedySchedulers/GreedyVarianceSspScheduler.hpp
@@ -75,7 +75,14 @@ class GreedyVarianceSspScheduler : public MaxBspScheduler<Graph_t> {
             }
             temp = std::log(temp) / 2 + max_priority;
 
-            double node_weight = std::log((double)std::max(graph.vertex_work_weight(*r_iter), static_cast<v_workw_t<Graph_t>>(1)));
+            double node_weight = std::log(
+                static_cast<double>(
+                    std::max(
+                        graph.vertex_work_weight(*r_iter),
+                        static_cast<v_workw_t<Graph_t>>(1)
+                    )
+                )
+            );
             double larger_val = node_weight > temp ? node_weight : temp;
 
             work_variance[*r_iter] =
